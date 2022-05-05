f1dfc50657926394bef855affe0b2326733b9ea4
Sam Deane <sam@elegantchaos.com>
2022-05-05 16:13:40 +0100

    Added optional creator parameter, and ability to delete all keys made by a creator.

--- Sources/Keychain/Keychain.swift
+++ Sources/Keychain/Keychain.swift
@@ -8,98 +8,125 @@ import Security
 
 public struct Keychain {
 
-    public static let `default` = Keychain()
-    
-    public enum KeychainError: Error {
-        case noPassword
-        case unhandledError(status: OSStatus)
-        case unexpectedPasswordData
-    }
-    
-    public init() {
-    }
-    
-    internal func query(for user: String, on server: String) -> NSDictionary {
-        let query: NSDictionary = [
-            kSecClass: kSecClassInternetPassword,
-            kSecAttrAccount: user,
-            kSecAttrServer: server,
-            kSecMatchLimit: kSecMatchLimitOne,
-            kSecReturnAttributes: true,
-            kSecReturnData: true
-        ]
+  public static let `default` = Keychain()
 
-        return query
-    }
-    
-    public func password(for user: String, on server: String) throws -> String {
-        let query = query(for: user, on: server)
-        
-        var item: CFTypeRef? = nil
-        let status = SecItemCopyMatching(query, &item)
-        guard status != errSecItemNotFound else { throw KeychainError.noPassword }
-        guard status == errSecSuccess else { throw KeychainError.unhandledError(status: status) }
-        
-        guard let existingItem = item as? [CFString : Any],
-            let passwordData = existingItem[kSecValueData] as? Data,
-            let password = String(data: passwordData, encoding: String.Encoding.utf8)
-            else {
-                throw KeychainError.unexpectedPasswordData
-        }
-        
-        return password
+  public enum KeychainError: Error {
+    case noPassword
+    case unhandledError(status: OSStatus)
+    case unexpectedPasswordData
+  }
+
+  public init() {
+  }
+
+  internal func query(for user: String, on server: String) -> NSDictionary {
+    let query: NSDictionary = [
+      kSecClass: kSecClassInternetPassword,
+      kSecAttrAccount: user,
+      kSecAttrServer: server,
+      kSecMatchLimit: kSecMatchLimitOne,
+      kSecReturnAttributes: true,
+      kSecReturnData: true,
+    ]
+
+    return query
+  }
+
+  public func password(for user: String, on server: String) throws -> String {
+    let query = query(for: user, on: server)
+
+    var item: CFTypeRef? = nil
+    let status = SecItemCopyMatching(query, &item)
+    guard status != errSecItemNotFound else { throw KeychainError.noPassword }
+    guard status == errSecSuccess else { throw KeychainError.unhandledError(status: status) }
+
+    guard let existingItem = item as? [CFString: Any],
+      let passwordData = existingItem[kSecValueData] as? Data,
+      let password = String(data: passwordData, encoding: String.Encoding.utf8)
+    else {
+      throw KeychainError.unexpectedPasswordData
     }
 
-    public func add(password: String, for user: String, on server: String) throws {
-        let tokenData = password.data(using: .utf8)!
-        let query: [String: Any] = [kSecClass as String: kSecClassInternetPassword,
-                                    kSecAttrAccount as String: user,
-                                    kSecAttrServer as String: server,
-                                    kSecValueData as String: tokenData]
-        let status = SecItemAdd(query as CFDictionary, nil)
-        guard status == errSecSuccess else { throw KeychainError.unhandledError(status: status) }
-    }
-    
-    public func update(password: String, for user: String, on server: String) throws {
-        let tokenData = password.data(using: .utf8)!
-        let query: [String: Any] = [
-            kSecClass as String: kSecClassInternetPassword,
-            kSecAttrAccount as String: user,
-            kSecAttrServer as String: server,
-        ]
+    return password
+  }
 
-        let update: [String: Any] = [
-            kSecValueData as String: tokenData
-        ]
-        
-        let status = SecItemUpdate(query as CFDictionary, update as CFDictionary)
-        switch status {
-            case errSecItemNotFound:
-                try add(password: password, for: user, on: server)
-                
-            case errSecSuccess:
-                break
-                
-            default:
-                throw KeychainError.unhandledError(status: status)
-        }
+  public func add(password: String, for user: String, on server: String, creator: UInt32? = nil)
+    throws
+  {
+    let tokenData = password.data(using: .utf8)!
+    var attributes: NSMutableDictionary = [
+      kSecClass: kSecClassInternetPassword,
+      kSecAttrAccount: user,
+      kSecAttrServer: server,
+      kSecValueData: tokenData,
+    ]
+
+    if let creator = creator {
+      attributes[kSecAttrCreator] = creator as CFNumber
     }
-    
-    public func delete(passwordFor user: String, on server: String) throws {
-        let query = query(for: user, on: server)
-        let status = SecItemDelete(query)
-        guard status == errSecSuccess || status == errSecItemNotFound else { throw KeychainError.unhandledError(status: status) }
+
+    let status = SecItemAdd(attributes as CFDictionary, nil)
+    guard status == errSecSuccess else { throw KeychainError.unhandledError(status: status) }
+  }
+
+  public func update(password: String, for user: String, on server: String) throws {
+    let tokenData = password.data(using: .utf8)!
+    let query: NSDictionary = [
+      kSecClass: kSecClassInternetPassword,
+      kSecAttrAccount: user,
+      kSecAttrServer: server,
+    ]
+
+    let update: NSDictionary = [
+      kSecValueData: tokenData
+    ]
+
+    let status = SecItemUpdate(query as CFDictionary, update as CFDictionary)
+    switch status {
+    case errSecItemNotFound:
+      try add(password: password, for: user, on: server)
+
+    case errSecSuccess:
+      break
+
+    default:
+      throw KeychainError.unhandledError(status: status)
     }
+  }
+
+  public func delete(passwordFor user: String, on server: String) throws {
+    let query = query(for: user, on: server)
+    let status = SecItemDelete(query)
+    guard status == errSecSuccess || status == errSecItemNotFound else {
+      throw KeychainError.unhandledError(status: status)
+    }
+  }
+
+  public func delete(allPasswordsCreatedBy appID: UInt32) throws {
+    let query: NSDictionary = [
+      kSecClass: kSecClassInternetPassword,
+      kSecAttrCreator: appID as CFNumber
+    ]
+
+    let status = SecItemDelete(query)
+    guard status == errSecSuccess || status == errSecItemNotFound else {
+      throw KeychainError.unhandledError(status: status)
+    }
+  }
 }
 
 /// Deprecated API
 
 extension Keychain {
-    @available(*, deprecated, message: "use add(password:for:on:) instead") public func addToken(_ token: String, user: String, server: String) throws {
-        try add(password: token, for: user, on: server)
-    }
+  @available(*, deprecated, message: "use add(password:for:on:) instead") public func addToken(
+    _ token: String, user: String, server: String
+  ) throws {
+    try add(password: token, for: user, on: server)
+  }
 
-    @available(*, deprecated, message: "use password(for:on:) instead") public func getToken(user: String, server: String) throws -> String {
-        return try password(for: user, on: server)
-    }
+  @available(*, deprecated, message: "use password(for:on:) instead") public func getToken(
+    user: String, server: String
+  ) throws -> String {
+    return try password(for: user, on: server)
+  }
 }
--- Tests/KeychainTests/KeychainTests.swift
+++ Tests/KeychainTests/KeychainTests.swift
@@ -2,6 +2,8 @@ import XCTest
 @testable import Keychain
 
 final class KeychainTests: XCTestCase {
+    static let exampleCreator : UInt32 = 0x75547374; /* corresponds to 'uTst' */
+
     func testAddAndRetrieve() {
         let password = UUID().uuidString
         let keychain = Keychain.default
@@ -9,6 +11,20 @@ final class KeychainTests: XCTestCase {
         do {
             try keychain.delete(passwordFor: "user", on: "server")
             try keychain.add(password: password, for: "user", on: "server")
+            let retrieved = try keychain.password(for: "user", on: "server")
+            XCTAssertEqual(password, retrieved)
+        } catch {
+            XCTFail("error: \(error)")
+        }
+    }
+
+    func testAddAndRetrieveWithCreator() {
+        let password = UUID().uuidString
+        let keychain = Keychain.default
+        
+        do {
+            try keychain.delete(passwordFor: "user", on: "server")
+            try keychain.add(password: password, for: "user", on: "server", creator: Self.exampleCreator)
             let retrieved = try keychain.password(for: "user", on: "server")
             XCTAssertEqual(password, retrieved)
         } catch {
@@ -27,6 +43,40 @@ final class KeychainTests: XCTestCase {
             try keychain.update(password: "new password", for: "user", on: "server")
             let retrieved = try keychain.password(for: "user", on: "server")
             XCTAssertEqual(retrieved, "new password")
+        } catch {
+            XCTFail("error: \(error)")
+        }
+    }
+
+    func testDelete() {
+        let server = UUID().uuidString
+        let password = UUID().uuidString
+        let keychain = Keychain.default
+        
+        do {
+            try keychain.add(password: password, for: "user", on: server)
+            let retrieved = try keychain.password(for: "user", on: server)
+            XCTAssertEqual(password, retrieved)
+            try keychain.delete(passwordFor: "user", on: server)
+            let missing = try? keychain.password(for: "user", on: server)
+            XCTAssertNil(missing)
+        } catch {
+            XCTFail("error: \(error)")
+        }
+    }
+
+    func testDeleteByCreator() {
+        let server = UUID().uuidString
+        let password = UUID().uuidString
+        let keychain = Keychain.default
+        
+        do {
+            try keychain.add(password: password, for: "user", on: server, creator: Self.exampleCreator)
+            let retrieved = try keychain.password(for: "user", on: server)
+            XCTAssertEqual(password, retrieved)
+            try keychain.delete(allPasswordsCreatedBy: Self.exampleCreator)
+            let missing = try? keychain.password(for: "user", on: server)
+            XCTAssertNil(missing)
         } catch {
             XCTFail("error: \(error)")
         }
