6779d6595a2e295eda11374c9653d39c765cc1c5
Sam Deane <sam@elegantchaos.com>
2022-05-06 11:06:00 +0100

    Cleaned up and simplified code. Removed deprecated API.

--- Sources/Keychain/Keychain.swift
+++ Sources/Keychain/Keychain.swift
@@ -7,134 +7,121 @@ import Foundation
 import Security
 
 public struct Keychain {
+    
+    public static let `default` = Keychain()
+    
+    public enum KeychainError: Error {
+        case unhandledError(status: OSStatus)
+        case unexpectedPasswordData
+    }
+    
+    /// The kind of items this object works with.
+    public let kind: CFString
 
-  public static let `default` = Keychain()
-
-  public enum KeychainError: Error {
-    case noPassword
-    case unhandledError(status: OSStatus)
-    case unexpectedPasswordData
-  }
-
-  public let kind: CFString
-  
-  public init(kind: CFString = kSecClassInternetPassword) {
-      self.kind = kind
-  }
-
-  internal func query(for user: String, on server: String) -> NSMutableDictionary {
-    let query: NSMutableDictionary = [
-      kSecClass: kind,
-      kSecAttrAccount: user,
-      kSecAttrServer: server,
-      kSecMatchLimit: kSecMatchLimitOne
-    ]
-
-    return query
-  }
-
-  public func password(for user: String, on server: String) throws -> String {
-    let query = query(for: user, on: server)
-    query[kSecReturnAttributes] = true
-    query[kSecReturnData] = true
-
-    var item: CFTypeRef? = nil
-    let status = SecItemCopyMatching(query, &item)
-    guard status != errSecItemNotFound else { throw KeychainError.noPassword }
-    guard status == errSecSuccess else { throw KeychainError.unhandledError(status: status) }
-
-    guard let existingItem = item as? [CFString: Any],
-      let passwordData = existingItem[kSecValueData] as? Data,
-      let password = String(data: passwordData, encoding: String.Encoding.utf8)
-    else {
-      throw KeychainError.unexpectedPasswordData
+    /// Creates an object for interacting with a certain type of keychain object.
+    public init(kind: CFString = kSecClassInternetPassword) {
+        self.kind = kind
+    }
+    
+    /// Returns a dictionary describing a keychain item.
+    internal func itemSpec(for user: String, on server: String, creator: UInt32? = nil) -> NSMutableDictionary {
+        let spec: NSMutableDictionary = [
+            kSecClass: kind,
+            kSecAttrAccount: user,
+            kSecAttrServer: server
+        ]
+        
+        if let creator = creator {
+            spec[creator] = creator as CFNumber
+        }
+        
+        return spec
+    }
+    
+    /// Returns the password entry for a given user/server pair.
+    /// If no password entry exists, we don't throw an error, but return nil.
+    /// Other keychain errors are wrapped and thrown.
+    public func password(for user: String, on server: String) throws -> String? {
+        let query = itemSpec(for: user, on: server)
+        query[kSecMatchLimit] = kSecMatchLimitOne
+        query[kSecReturnAttributes] = true
+        query[kSecReturnData] = true
+        
+        var item: CFTypeRef? = nil
+        let status = SecItemCopyMatching(query, &item)
+        guard status != errSecItemNotFound else { return nil }
+        guard status == errSecSuccess else { throw KeychainError.unhandledError(status: status) }
+        
+        guard let existingItem = item as? [CFString: Any],
+              let passwordData = existingItem[kSecValueData] as? Data,
+              let password = String(data: passwordData, encoding: String.Encoding.utf8)
+        else {
+            throw KeychainError.unexpectedPasswordData
+        }
+        
+        return password
+    }
+    
+    /// Add a new password entry for a user/server pair.
+    /// We optionally take a creator code which can be used later to delete any matching passwords.
+    public func add(password: String, for user: String, on server: String, creator: UInt32? = nil) throws {
+        let tokenData = password.data(using: .utf8)!
+        let attributes = itemSpec(for: user, on: server, creator: creator)
+        attributes[kSecValueData] = tokenData
+        
+        let status = SecItemAdd(attributes, nil)
+        guard status == errSecSuccess else { throw KeychainError.unhandledError(status: status) }
+    }
+    
+    /// Update an existing password entry if it exists.
+    /// If no entry existed, we create one.
+    public func update(password: String, for user: String, on server: String, creator: UInt32? = nil) throws {
+        let tokenData = password.data(using: .utf8)!
+        let query = itemSpec(for: user, on: server, creator: creator)
+        let update: NSDictionary = [
+            kSecValueData: tokenData
+        ]
+        
+        let status = SecItemUpdate(query, update)
+        switch status {
+            case errSecItemNotFound:
+                try add(password: password, for: user, on: server, creator: creator)
+                
+            case errSecSuccess:
+                break
+                
+            default:
+                throw KeychainError.unhandledError(status: status)
+        }
+    }
+    
+    /// Remove the first keychain entry for a given user/server (and optionally, creator).
+    /// If the entry didn't exist, we don't throw any errors.
+    public func delete(passwordFor user: String, on server: String, creator: UInt32? = nil) throws {
+        let query = itemSpec(for: user, on: server, creator: creator)
+        query[kSecMatchLimit] = kSecMatchLimitOne
+        let status = SecItemDelete(query)
+        guard status == errSecSuccess || status == errSecItemNotFound else {
+            throw KeychainError.unhandledError(status: status)
+        }
     }
 
-    return password
-  }
-
-  public func add(password: String, for user: String, on server: String, creator: UInt32? = nil)
-    throws
-  {
-    let tokenData = password.data(using: .utf8)!
-    let attributes: NSMutableDictionary = [
-      kSecClass: kind,
-      kSecAttrAccount: user,
-      kSecAttrServer: server,
-      kSecValueData: tokenData,
-    ]
-
-    if let creator = creator {
-      attributes[kSecAttrCreator] = creator as CFNumber
+    /// Delete all password entries with a given creator tag.
+    /// The creator code is a 32 bit number, generally interpreted as an
+    /// old-school 4-char OSType. If you tag any entries that you create
+    /// with a creator, you can later use this call to remove them all.
+    /// Entries created by the user in some other way are unlikely to have
+    /// the same creator, so should be left untouched.
+    public func delete(allPasswordsCreatedBy appID: UInt32) throws {
+        let query: NSDictionary = [
+            kSecClass: kind,
+            kSecAttrCreator: appID as CFNumber,
+            kSecMatchLimit: kSecMatchLimitAll
+        ]
+        
+        let status = SecItemDelete(query)
+        guard status == errSecSuccess || status == errSecItemNotFound else {
+            throw KeychainError.unhandledError(status: status)
+        }
     }
-
-    let status = SecItemAdd(attributes as CFDictionary, nil)
-    guard status == errSecSuccess else { throw KeychainError.unhandledError(status: status) }
-  }
-
-  public func update(password: String, for user: String, on server: String, creator: UInt32? = nil) throws {
-    let tokenData = password.data(using: .utf8)!
-      var query: [CFString:Any] = [
-      kSecClass: kSecClassInternetPassword,
-      kSecAttrAccount: user,
-      kSecAttrServer: server
-    ]
-
-    if let creator = creator {
-      query[kSecAttrCreator] = creator as CFNumber
-    }
-
-      let update: [String:Any] = [
-      kSecValueData as String: tokenData
-    ]
-
-    let status = SecItemUpdate(query as CFDictionary, update as CFDictionary)
-    switch status {
-    case errSecItemNotFound:
-      try add(password: password, for: user, on: server, creator: creator)
-
-    case errSecSuccess:
-      break
-
-    default:
-      throw KeychainError.unhandledError(status: status)
-    }
-  }
-
-  public func delete(passwordFor user: String, on server: String) throws {
-    let query = query(for: user, on: server)
-    let status = SecItemDelete(query)
-    guard status == errSecSuccess || status == errSecItemNotFound else {
-      throw KeychainError.unhandledError(status: status)
-    }
-  }
-
-  public func delete(allPasswordsCreatedBy appID: UInt32) throws {
-    let query: NSDictionary = [
-      kSecClass: kSecClassInternetPassword,
-      kSecAttrCreator: appID as CFNumber,
-      kSecMatchLimit: kSecMatchLimitAll
-    ]
-
-    let status = SecItemDelete(query)
-    guard status == errSecSuccess || status == errSecItemNotFound else {
-      throw KeychainError.unhandledError(status: status)
-    }
-  }
-}
-
-/// Deprecated API
-
-extension Keychain {
-  @available(*, deprecated, message: "use add(password:for:on:) instead") public func addToken(
-    _ token: String, user: String, server: String
-  ) throws {
-    try add(password: token, for: user, on: server)
-  }
-
-  @available(*, deprecated, message: "use password(for:on:) instead") public func getToken(
-    user: String, server: String
-  ) throws -> String {
-    return try password(for: user, on: server)
-  }
 }
