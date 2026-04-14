5639a12fbbe7197495740ba936ddbe3e2ba04182
Sam Deane <sam@elegantchaos.com>
2026-04-14 13:17:48 +0100

    updated CI

--- Package.swift
+++ Package.swift
@@ -8,20 +8,24 @@
 import PackageDescription
 
 let package = Package(
-    name: "Keychain",
-    products: [
-        .library(
-            name: "Keychain",
-            targets: ["Keychain"]),
-    ],
-    dependencies: [
-    ],
-    targets: [
-        .target(
-            name: "Keychain",
-            dependencies: []),
-        .testTarget(
-            name: "KeychainTests",
-            dependencies: ["Keychain"]),
-    ]
+  name: "Keychain",
+  products: [
+    .library(
+      name: "Keychain",
+      targets: ["Keychain"])
+  ],
+  dependencies: [
+    .package(
+      url: "https://github.com/elegantchaos/ActionBuilderPlugin.git",
+      from: "2.0.3"
+    )
+  ],
+  targets: [
+    .target(
+      name: "Keychain",
+      dependencies: []),
+    .testTarget(
+      name: "KeychainTests",
+      dependencies: ["Keychain"]),
+  ]
 )
