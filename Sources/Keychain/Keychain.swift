1ffa2ef109c0b773f0c96b315608e91fd26e500f
Sam Deane <sam@elegantchaos.com>
2026-04-14 16:46:36 +0100

    Added optional access group

--- Sources/Keychain/Keychain.swift
+++ Sources/Keychain/Keychain.swift
@@ -8,153 +8,171 @@ import Security
 
 public struct Keychain {
 
-    public static let `default` = Keychain()
+  public static let `default` = Keychain()
 
-    public enum KeychainError: Error {
-        case unhandledError(status: OSStatus)
-        case unexpectedPasswordData
+  public enum KeychainError: Error {
+    case unhandledError(status: OSStatus)
+    case unexpectedPasswordData
+  }
+
+  /// Selects which macOS keychain backend this value should target.
+  public enum Storage: Sendable {
+    /// Use the historical file-based keychain implementation.
+    case fileBased
+
+    /// Use the data protection keychain on supported macOS systems.
+    case dataProtection
+  }
+
+  /// The kind of items this object works with.
+  public let kind: CFString
+
+  /// The macOS keychain backend this object should target.
+  public let storage: Storage
+
+  /// Optional access group used when targeting the data protection keychain.
+  public let accessGroup: String?
+
+  /// Creates an object for interacting with a certain type of keychain object.
+  /// Defaults to the modern data protection keychain backend on macOS.
+  public init(kind: CFString = kSecClassInternetPassword, storage: Storage = .dataProtection, accessGroup: String? = nil) {
+    self.kind = kind
+    self.storage = storage
+    self.accessGroup = accessGroup
+  }
+
+  /// Returns the primary location attribute key used by the configured item class.
+  internal var locationKey: CFString {
+    switch kind {
+      case kSecClassGenericPassword:
+        kSecAttrService
+      default:
+        kSecAttrServer
+    }
+  }
+
+  /// Returns a dictionary describing a keychain item.
+  internal func itemSpec(for user: String, on server: String, creator: UInt32? = nil) -> NSMutableDictionary {
+    let spec: NSMutableDictionary = [
+      kSecClass: kind,
+      kSecAttrAccount: user,
+      locationKey: server,
+    ]
+
+    if let creator = creator {
+      spec[kSecAttrCreator] = creator as CFNumber
     }
 
-    /// Selects which macOS keychain backend this value should target.
-    public enum Storage: Sendable {
-        /// Use the historical file-based keychain implementation.
-        case fileBased
-
-        /// Use the data protection keychain on supported macOS systems.
-        case dataProtection
+    if let accessGroup {
+      spec[kSecAttrAccessGroup] = accessGroup
     }
 
-    /// The kind of items this object works with.
-    public let kind: CFString
-
-    /// The macOS keychain backend this object should target.
-    public let storage: Storage
-
-    /// Creates an object for interacting with a certain type of keychain object.
-    /// Defaults to the modern data protection keychain backend on macOS.
-    public init(kind: CFString = kSecClassInternetPassword, storage: Storage = .dataProtection) {
-        self.kind = kind
-        self.storage = storage
-    }
-
-    /// Returns a dictionary describing a keychain item.
-    internal func itemSpec(for user: String, on server: String, creator: UInt32? = nil) -> NSMutableDictionary {
-        let spec: NSMutableDictionary = [
-            kSecClass: kind,
-            kSecAttrAccount: user,
-            kSecAttrServer: server
-        ]
-        
-        if let creator = creator {
-            spec[kSecAttrCreator] = creator as CFNumber
+    #if os(macOS)
+      if storage == .dataProtection {
+        if #available(macOS 10.15, *) {
+          spec[kSecUseDataProtectionKeychain] = true as CFBoolean
         }
+      }
+    #endif
 
-        #if os(macOS)
-            if storage == .dataProtection {
-                if #available(macOS 10.15, *) {
-                    spec[kSecUseDataProtectionKeychain] = true as CFBoolean
-                }
-            }
-        #endif
-        
-        return spec
-    }
-    
-    /// Returns the password entry for a given user/server pair.
-    /// If no password entry exists, we don't throw an error, but return nil.
-    /// Other keychain errors are wrapped and thrown.
-    public func password(for user: String, on server: String) throws -> String? {
-        let query = itemSpec(for: user, on: server)
-        query[kSecMatchLimit] = kSecMatchLimitOne
-        query[kSecReturnAttributes] = true
-        query[kSecReturnData] = true
-        
-        var item: CFTypeRef? = nil
-        let status = SecItemCopyMatching(query, &item)
-        guard status != errSecItemNotFound else { return nil }
-        guard status == errSecSuccess else { throw KeychainError.unhandledError(status: status) }
-        
-        guard let existingItem = item as? [CFString: Any],
-              let passwordData = existingItem[kSecValueData] as? Data,
-              let password = String(data: passwordData, encoding: String.Encoding.utf8)
-        else {
-            throw KeychainError.unexpectedPasswordData
-        }
-        
-        return password
-    }
-    
-    /// Add a new password entry for a user/server pair.
-    /// We optionally take a creator code which can be used later to delete any matching passwords.
-    public func add(password: String, for user: String, on server: String, creator: UInt32? = nil) throws {
-        let tokenData = password.data(using: .utf8)!
-        let attributes = itemSpec(for: user, on: server, creator: creator)
-        attributes[kSecValueData] = tokenData
-        
-        let status = SecItemAdd(attributes, nil)
-        guard status == errSecSuccess else { throw KeychainError.unhandledError(status: status) }
-    }
-    
-    /// Update an existing password entry if it exists.
-    /// If no entry existed, we create one.
-    public func update(password: String, for user: String, on server: String, creator: UInt32? = nil) throws {
-        let tokenData = password.data(using: .utf8)!
-        let query = itemSpec(for: user, on: server, creator: creator)
-        let update: NSDictionary = [
-            kSecValueData: tokenData
-        ]
-        
-        let status = SecItemUpdate(query, update)
-        switch status {
-            case errSecItemNotFound:
-                try add(password: password, for: user, on: server, creator: creator)
-                
-            case errSecSuccess:
-                break
-                
-            default:
-                throw KeychainError.unhandledError(status: status)
-        }
-    }
-    
-    /// Remove the first keychain entry for a given user/server (and optionally, creator).
-    /// If the entry didn't exist, we don't throw any errors.
-    public func delete(passwordFor user: String, on server: String, creator: UInt32? = nil) throws {
-        let query = itemSpec(for: user, on: server, creator: creator)
-        let status = SecItemDelete(query)
-        guard status == errSecSuccess || status == errSecItemNotFound else {
-            throw KeychainError.unhandledError(status: status)
-        }
+    return spec
+  }
+
+  /// Returns the password entry for a given user/server pair.
+  /// If no password entry exists, we don't throw an error, but return nil.
+  /// Other keychain errors are wrapped and thrown.
+  public func password(for user: String, on server: String) throws -> String? {
+    let query = itemSpec(for: user, on: server)
+    query[kSecMatchLimit] = kSecMatchLimitOne
+    query[kSecReturnAttributes] = true
+    query[kSecReturnData] = true
+
+    var item: CFTypeRef? = nil
+    let status = SecItemCopyMatching(query, &item)
+    guard status != errSecItemNotFound else { return nil }
+    guard status == errSecSuccess else { throw KeychainError.unhandledError(status: status) }
+
+    guard let existingItem = item as? [CFString: Any],
+      let passwordData = existingItem[kSecValueData] as? Data,
+      let password = String(data: passwordData, encoding: String.Encoding.utf8)
+    else {
+      throw KeychainError.unexpectedPasswordData
     }
 
-    /// Delete all password entries with a given creator tag.
-    /// The creator code is a 32 bit number, generally interpreted as an
-    /// old-school 4-char OSType. If you tag any entries that you create
-    /// with a creator, you can later use this call to remove them all.
-    /// Entries created by the user in some other way are unlikely to have
-    /// the same creator, so should be left untouched.
-    public func delete(allPasswordsCreatedBy appID: UInt32) throws {
-        let query: NSMutableDictionary = [
-            kSecClass: kind,
-            kSecAttrCreator: appID as CFNumber
-        ]
+    return password
+  }
 
-        #if os(macOS)
-            //
-            query[kSecMatchLimit] = kSecMatchLimitAll
-        #endif
+  /// Add a new password entry for a user/server pair.
+  /// We optionally take a creator code which can be used later to delete any matching passwords.
+  public func add(password: String, for user: String, on server: String, creator: UInt32? = nil) throws {
+    let tokenData = password.data(using: .utf8)!
+    let attributes = itemSpec(for: user, on: server, creator: creator)
+    attributes[kSecValueData] = tokenData
 
-        var item: CFTypeRef? = nil
-        repeat  {
-            var status = SecItemCopyMatching(query, &item)
-            if status == errSecItemNotFound {
-                return
-            }
-            
-            status = SecItemDelete(query)
-            guard status == errSecSuccess || status == errSecItemNotFound else {
-                throw KeychainError.unhandledError(status: status)
-            }
-        } while(true)
+    let status = SecItemAdd(attributes, nil)
+    guard status == errSecSuccess else { throw KeychainError.unhandledError(status: status) }
+  }
+
+  /// Update an existing password entry if it exists.
+  /// If no entry existed, we create one.
+  public func update(password: String, for user: String, on server: String, creator: UInt32? = nil) throws {
+    let tokenData = password.data(using: .utf8)!
+    let query = itemSpec(for: user, on: server, creator: creator)
+    let update: NSDictionary = [
+      kSecValueData: tokenData
+    ]
+
+    let status = SecItemUpdate(query, update)
+    switch status {
+      case errSecItemNotFound:
+        try add(password: password, for: user, on: server, creator: creator)
+
+      case errSecSuccess:
+        break
+
+      default:
+        throw KeychainError.unhandledError(status: status)
     }
+  }
+
+  /// Remove the first keychain entry for a given user/server (and optionally, creator).
+  /// If the entry didn't exist, we don't throw any errors.
+  public func delete(passwordFor user: String, on server: String, creator: UInt32? = nil) throws {
+    let query = itemSpec(for: user, on: server, creator: creator)
+    let status = SecItemDelete(query)
+    guard status == errSecSuccess || status == errSecItemNotFound else {
+      throw KeychainError.unhandledError(status: status)
+    }
+  }
+
+  /// Delete all password entries with a given creator tag.
+  /// The creator code is a 32 bit number, generally interpreted as an
+  /// old-school 4-char OSType. If you tag any entries that you create
+  /// with a creator, you can later use this call to remove them all.
+  /// Entries created by the user in some other way are unlikely to have
+  /// the same creator, so should be left untouched.
+  public func delete(allPasswordsCreatedBy appID: UInt32) throws {
+    let query: NSMutableDictionary = [
+      kSecClass: kind,
+      kSecAttrCreator: appID as CFNumber,
+    ]
+
+    #if os(macOS)
+      //
+      query[kSecMatchLimit] = kSecMatchLimitAll
+    #endif
+
+    var item: CFTypeRef? = nil
+    repeat {
+      var status = SecItemCopyMatching(query, &item)
+      if status == errSecItemNotFound {
+        return
+      }
+
+      status = SecItemDelete(query)
+      guard status == errSecSuccess || status == errSecItemNotFound else {
+        throw KeychainError.unhandledError(status: status)
+      }
+    } while true
+  }
 }
--- Tests/KeychainTests/KeychainTests.swift
+++ Tests/KeychainTests/KeychainTests.swift
@@ -1,138 +1,156 @@
 import Security
 import XCTest
+
 @testable import Keychain
 
 final class KeychainTests: XCTestCase {
-    static let exampleCreator : UInt32 = 0x75547374; /* corresponds to 'uTst' */
+  static let exampleCreator: UInt32 = 0x7554_7374 /* corresponds to 'uTst' */
 
-    private func makeExecutableTestKeychain() -> Keychain {
-        #if os(macOS)
-            // `swift test` runs without the entitlement required by the
-            // data protection backend, so integration-style tests use the
-            // file-based backend while separate assertions cover the default.
-            Keychain(storage: .fileBased)
-        #else
-            .default
-        #endif
-    }
-
+  private func makeExecutableTestKeychain() -> Keychain {
     #if os(macOS)
+      // `swift test` runs without the entitlement required by the
+      // data protection backend, so integration-style tests use the
+      // file-based backend while separate assertions cover the default.
+      Keychain(storage: .fileBased)
+    #else
+      .default
+    #endif
+  }
+
+  #if os(macOS)
     func testDefaultStorageUsesDataProtection() throws {
-        guard #available(macOS 10.15, *) else { return }
-        let keychain = Keychain()
+      guard #available(macOS 10.15, *) else { return }
+      let keychain = Keychain()
 
-        let spec = keychain.itemSpec(for: "unittest", on: "server")
+      let spec = keychain.itemSpec(for: "unittest", on: "server")
 
-        XCTAssertEqual(spec[kSecUseDataProtectionKeychain] as? Bool, true)
+      XCTAssertEqual(spec[kSecUseDataProtectionKeychain] as? Bool, true)
     }
 
     func testDataProtectionStorageAddsBackendFlag() throws {
-        guard #available(macOS 10.15, *) else { return }
-        let keychain = Keychain(storage: .dataProtection)
+      guard #available(macOS 10.15, *) else { return }
+      let keychain = Keychain(storage: .dataProtection)
 
-        let spec = keychain.itemSpec(for: "unittest", on: "server")
+      let spec = keychain.itemSpec(for: "unittest", on: "server")
 
-        XCTAssertEqual(spec[kSecUseDataProtectionKeychain] as? Bool, true)
+      XCTAssertEqual(spec[kSecUseDataProtectionKeychain] as? Bool, true)
+    }
+
+    func testAccessGroupIsAddedWhenProvided() throws {
+      let keychain = Keychain(storage: .dataProtection, accessGroup: "group.com.elegantchaos.actionstatus")
+
+      let spec = keychain.itemSpec(for: "unittest", on: "server")
+
+      XCTAssertEqual(spec[kSecAttrAccessGroup] as? String, "group.com.elegantchaos.actionstatus")
+    }
+
+    func testGenericPasswordUsesServiceAttribute() throws {
+      let keychain = Keychain(kind: kSecClassGenericPassword, storage: .dataProtection)
+
+      let spec = keychain.itemSpec(for: "unittest", on: "service")
+
+      XCTAssertEqual(spec[kSecAttrService] as? String, "service")
+      XCTAssertNil(spec[kSecAttrServer])
     }
 
     func testFileBasedStorageOmitsBackendFlag() throws {
-        guard #available(macOS 10.15, *) else { return }
-        let keychain = Keychain(storage: .fileBased)
+      guard #available(macOS 10.15, *) else { return }
+      let keychain = Keychain(storage: .fileBased)
 
-        let spec = keychain.itemSpec(for: "unittest", on: "server")
+      let spec = keychain.itemSpec(for: "unittest", on: "server")
 
-        XCTAssertNil(spec[kSecUseDataProtectionKeychain])
+      XCTAssertNil(spec[kSecUseDataProtectionKeychain])
     }
-    #endif
+  #endif
 
-    func testAddAndRetrieve() throws {
-        let password = UUID().uuidString
-        let keychain = makeExecutableTestKeychain()
-        
-        try keychain.delete(passwordFor: "unittest", on: "server")
-        try keychain.add(password: password, for: "unittest", on: "server")
-        let retrieved = try keychain.password(for: "unittest", on: "server")
-        XCTAssertEqual(password, retrieved)
-    }
+  func testAddAndRetrieve() throws {
+    let password = UUID().uuidString
+    let keychain = makeExecutableTestKeychain()
 
-    func testAddUsingUpdate() throws {
-        let password = UUID().uuidString
-        let keychain = makeExecutableTestKeychain()
+    try keychain.delete(passwordFor: "unittest", on: "server")
+    try keychain.add(password: password, for: "unittest", on: "server")
+    let retrieved = try keychain.password(for: "unittest", on: "server")
+    XCTAssertEqual(password, retrieved)
+  }
 
-        try keychain.delete(passwordFor: "unittest", on: "server")
-        try keychain.update(password: password, for: "unittest", on: "server")
-        let retrieved = try keychain.password(for: "unittest", on: "server")
-        XCTAssertEqual(password, retrieved)
-    }
+  func testAddUsingUpdate() throws {
+    let password = UUID().uuidString
+    let keychain = makeExecutableTestKeychain()
 
-    func testAddAndRetrieveWithCreator() throws {
-        let password = UUID().uuidString
-        let keychain = makeExecutableTestKeychain()
-        
-        try keychain.delete(passwordFor: "unittest", on: "server")
-        try keychain.add(password: password, for: "unittest", on: "server", creator: Self.exampleCreator)
-        let retrieved = try keychain.password(for: "unittest", on: "server")
-        XCTAssertEqual(password, retrieved)
-    }
+    try keychain.delete(passwordFor: "unittest", on: "server")
+    try keychain.update(password: password, for: "unittest", on: "server")
+    let retrieved = try keychain.password(for: "unittest", on: "server")
+    XCTAssertEqual(password, retrieved)
+  }
 
-    func testAddUsingUpdateWithCreator() throws {
-        let password = UUID().uuidString
-        let keychain = makeExecutableTestKeychain()
-        
-        try keychain.delete(passwordFor: "unittest", on: "server")
-        try keychain.update(password: password, for: "unittest", on: "server", creator: Self.exampleCreator)
-        let retrieved = try keychain.password(for: "unittest", on: "server")
-        XCTAssertEqual(password, retrieved)
-    }
+  func testAddAndRetrieveWithCreator() throws {
+    let password = UUID().uuidString
+    let keychain = makeExecutableTestKeychain()
 
-    func testUpdate() throws {
-        let password = UUID().uuidString
-        let keychain = makeExecutableTestKeychain()
-        
-        try keychain.delete(passwordFor: "unittest", on: "server")
-        try keychain.add(password: password, for: "unittest", on: "server")
-        try keychain.update(password: "new password", for: "unittest", on: "server")
-        let retrieved = try keychain.password(for: "unittest", on: "server")
-        XCTAssertEqual(retrieved, "new password")
-    }
+    try keychain.delete(passwordFor: "unittest", on: "server")
+    try keychain.add(password: password, for: "unittest", on: "server", creator: Self.exampleCreator)
+    let retrieved = try keychain.password(for: "unittest", on: "server")
+    XCTAssertEqual(password, retrieved)
+  }
+
+  func testAddUsingUpdateWithCreator() throws {
+    let password = UUID().uuidString
+    let keychain = makeExecutableTestKeychain()
+
+    try keychain.delete(passwordFor: "unittest", on: "server")
+    try keychain.update(password: password, for: "unittest", on: "server", creator: Self.exampleCreator)
+    let retrieved = try keychain.password(for: "unittest", on: "server")
+    XCTAssertEqual(password, retrieved)
+  }
+
+  func testUpdate() throws {
+    let password = UUID().uuidString
+    let keychain = makeExecutableTestKeychain()
+
+    try keychain.delete(passwordFor: "unittest", on: "server")
+    try keychain.add(password: password, for: "unittest", on: "server")
+    try keychain.update(password: "new password", for: "unittest", on: "server")
+    let retrieved = try keychain.password(for: "unittest", on: "server")
+    XCTAssertEqual(retrieved, "new password")
+  }
 
 
-    func testUpdateWithCreator() throws {
-        let password = UUID().uuidString
-        let keychain = makeExecutableTestKeychain()
-        
-        try keychain.delete(passwordFor: "unittest", on: "server")
-        try keychain.add(password: password, for: "unittest", on: "server", creator: Self.exampleCreator)
-        try keychain.update(password: "new password", for: "unittest", on: "server", creator: Self.exampleCreator)
-        let retrieved = try keychain.password(for: "unittest", on: "server")
-        XCTAssertEqual(retrieved, "new password")
-    }
+  func testUpdateWithCreator() throws {
+    let password = UUID().uuidString
+    let keychain = makeExecutableTestKeychain()
 
-    func testDelete() throws {
-        let server = UUID().uuidString
-        let password = UUID().uuidString
-        let keychain = makeExecutableTestKeychain()
-        
-        try keychain.add(password: password, for: "unittest", on: server)
-        let retrieved = try keychain.password(for: "unittest", on: server)
-        XCTAssertEqual(password, retrieved)
-        try keychain.delete(passwordFor: "unittest", on: server)
-        let missing = try? keychain.password(for: "unittest", on: server)
-        XCTAssertNil(missing)
-    }
+    try keychain.delete(passwordFor: "unittest", on: "server")
+    try keychain.add(password: password, for: "unittest", on: "server", creator: Self.exampleCreator)
+    try keychain.update(password: "new password", for: "unittest", on: "server", creator: Self.exampleCreator)
+    let retrieved = try keychain.password(for: "unittest", on: "server")
+    XCTAssertEqual(retrieved, "new password")
+  }
 
-    func testDeleteByCreator() throws {
-        let server = UUID().uuidString
-        let password = UUID().uuidString
-        let keychain = makeExecutableTestKeychain()
-        
-        try keychain.add(password: password, for: "unittest1", on: server, creator: Self.exampleCreator)
-        try keychain.add(password: password, for: "unittest2", on: server, creator: Self.exampleCreator)
-        XCTAssertEqual(password, try keychain.password(for: "unittest1", on: server))
-        XCTAssertEqual(password, try keychain.password(for: "unittest2", on: server))
+  func testDelete() throws {
+    let server = UUID().uuidString
+    let password = UUID().uuidString
+    let keychain = makeExecutableTestKeychain()
 
-        try keychain.delete(allPasswordsCreatedBy: Self.exampleCreator)
-        XCTAssertNil(try? keychain.password(for: "unittest1", on: server))
-        XCTAssertNil(try? keychain.password(for: "unittest2", on: server))
-    }
+    try keychain.add(password: password, for: "unittest", on: server)
+    let retrieved = try keychain.password(for: "unittest", on: server)
+    XCTAssertEqual(password, retrieved)
+    try keychain.delete(passwordFor: "unittest", on: server)
+    let missing = try? keychain.password(for: "unittest", on: server)
+    XCTAssertNil(missing)
+  }
+
+  func testDeleteByCreator() throws {
+    let server = UUID().uuidString
+    let password = UUID().uuidString
+    let keychain = makeExecutableTestKeychain()
+
+    try keychain.add(password: password, for: "unittest1", on: server, creator: Self.exampleCreator)
+    try keychain.add(password: password, for: "unittest2", on: server, creator: Self.exampleCreator)
+    XCTAssertEqual(password, try keychain.password(for: "unittest1", on: server))
+    XCTAssertEqual(password, try keychain.password(for: "unittest2", on: server))
+
+    try keychain.delete(allPasswordsCreatedBy: Self.exampleCreator)
+    XCTAssertNil(try? keychain.password(for: "unittest1", on: server))
+    XCTAssertNil(try? keychain.password(for: "unittest2", on: server))
+  }
 }
